919186e2ce5fa85dbe94cbd42a1d6bb283d7c436
theMahammad <mehemmedsadigzade@gmail.com>
2023-03-08 16:29:00 +0400

    Registration page has been integrated with the project

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -11,8 +11,8 @@ namespace EntityLayer.Concrete
         [Key]
         public int ID { get; set; }
         public string Name { get; set; }
-        public string About { get; set; }
-        public string Image { get; set; }
+        public string? About { get; set; }
+        public string? Image { get; set; }
         public string EmailAdress { get; set; }
         public string Password { get; set; }
         public bool Status { get; set; }
---
+++ PresentationLayer/Controllers/RegistrationController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        WriterManager manager = new WriterManager(new EfWriterRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register(Writer writer)
+        {
+
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
