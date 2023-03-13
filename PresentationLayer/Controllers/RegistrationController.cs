360349b7f37e6c4ec9bd4076da58b2ab8a986a77
theMahammad <mehemmedsadigzade@gmail.com>
2023-03-13 15:54:19 +0400

    Using of RegistryViewModel and Html.DropDownListFor

--- PresentationLayer/Controllers/RegistrationController.cs
+++ PresentationLayer/Controllers/RegistrationController.cs
@@ -2,16 +2,22 @@
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
-
+using PresentationLayer.Models;
 namespace PresentationLayer.Controllers
 {
     public class RegistrationController : Controller
     {
         WriterManager manager = new WriterManager(new EfWriterRepository());
+        CategoryManager c_manager = new CategoryManager(new EfCategoryRepository());
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var RegistryViewModel = new RegistryViewModel
+            {
+                Writer = new Writer(),
+                Categories = c_manager.GetAll()
+            };
+            return View(RegistryViewModel);
         }
         [HttpPost]
         public IActionResult Index(Writer writer)
---
+++ PresentationLayer/Models/RegistryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using EntityLayer.Concrete;
+
+namespace PresentationLayer.Models
+{
+	public class RegistryViewModel
+	{
+		public Writer Writer { get; set; }
+		public List<Category> Categories { get; set; }
+	}
+}
